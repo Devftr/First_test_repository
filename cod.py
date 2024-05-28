1435abd11d01f7bb100feca42e9a9c33d003581a
Devftr <felpsTR67@gmail.com>
2024-05-28 18:32:59 -0300

    Código-apresentação

---
+++ cod.py
@@ -0,0 +1,14 @@
+print('Esse é meu primeiro repositorio no Github!')
+
+informacoes = {
+
+    'Nome': 'Fellipe, mas quero ser conhecido como dev-Ftr ',
+    'Idade': 'Tenho 19 anos de idade',
+    'Formação': '3º semestre em gestão de tecnologia da informação',
+    'Ambição': 'Ser um dev full stack em python',
+    'Interesses': 'Tenho interesse em encontrar um emprego que me proporcione liberdade para colocar em pratica meus conhecimentos!'
+
+}
+
+#e tomi
+
